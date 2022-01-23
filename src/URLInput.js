81ccdee59415b587a65bc7f2399ba922bc01dc8c
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-01-23 16:08:48 -0500

    Sent a request after clicking shorten it button.

--- src/URLInput.js
+++ src/URLInput.js
@@ -1,13 +1,41 @@
 import React from "react";
+import axios from "axios";
 import "./URLInput.css";
 
 class URLInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.urlInput = React.createRef();
+  }
+
+  shorten() {
+    console.log(`URL: ${this.urlInput.current.value}`);
+    const url = `https://api.shrtco.de/v2/shorten?url=${this.urlInput.current.value}`;
+    axios
+      .post(url)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <div>
         <div className="input-container">
-          <input placeholder="Shorten a link here..."></input>
-          <button>Shorten It!</button>
+          <input
+            ref={this.urlInput}
+            placeholder="Shorten a link here..."
+          ></input>
+          <button
+            onClick={() => {
+              this.shorten();
+            }}
+          >
+            Shorten It!
+          </button>
         </div>
       </div>
     );
