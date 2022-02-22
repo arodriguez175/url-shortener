e9d0f5dc7208389e309c3b80f217ad5d460b870c
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-02-21 22:57:57 -0500

    Gave Home background two different colors to match the design.

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "./Hero";
 import URLShortener from "./features/urlShortener/URLShortener"; //
+import "./Home.css";
 
 class Home extends React.Component {
   render() {
