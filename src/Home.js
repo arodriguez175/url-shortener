e935396686154d65aef35e7783a523100ceb0d67
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-03-11 23:39:28 -0500

    Fixed split colored background showing up on Pricing page.

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,7 @@ import Footer from "./Footer";
 class Home extends React.Component {
   render() {
     return (
-      <div>
+      <div className="home">
         <Hero />
         <URLShortener />
         <AdvancedStatistics />
