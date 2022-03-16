fc1cd4b78589d14fccca70de0965dc20478a487c
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-03-16 13:41:52 -0400

    Added more comments for notes.

--- src/features/urlShortener/shortenerSlice.js
+++ src/features/urlShortener/shortenerSlice.js
@@ -37,12 +37,37 @@ export const shortenerSlice = createSlice({
 });
 
 export function shortenUrl(originalUrl) {
+  // async makes a function return a Promise.
   return async (dispatch) => {
+    /* assigning url as the shrtco API base url 
+    and assigning that to my original url. */
     const url = `https://api.shrtco.de/v2/shorten?url=${originalUrl}`;
+
+    /* dispatch is a function of the Redux store and 
+    is used to dispatch an action. 
+    This is the only way to trigger a state change.
+    
+    setLoading is just a variable name for React's built-in 
+    useState hook. 
+    This setter function can be called anything. 
+    Used to describe the status of async requests for 
+    the purpose of updating the UI to show that the request is 
+    happening. (eg put up a spinner, disable submit buttons, etc) */
     dispatch(setLoading(true));
+
+    /* axios is a promise based HTTP client 
+    for the browser and node.js that is able to send 
+    a request to and get a response from the server in 
+    HTTP format. */
     axios
+      /* post sends data to the API server to create or update a resource. 
+      Sends my url data to the axios server. */
       .post(url)
+
+      /* then returns a promise that takes two arguments: callback functions for 
+      the success and failure cases of the promise. */
       .then((response) => {
+        //axios response data.
         /* response: {
           data: {...},
           ...
@@ -50,8 +75,11 @@ export function shortenUrl(originalUrl) {
         const data = response.data;
         const shortenedUrlObject = {
           originalUrl: originalUrl,
+          // full short link from response data.
           shortenedUrl: data.result.full_short_link,
         };
+
+        // Dispatch action to save shortened url.
         dispatch(saveShortenedUrl(shortenedUrlObject));
       })
       .catch(function (error) {
@@ -63,6 +91,8 @@ export function shortenUrl(originalUrl) {
   };
 }
 
+// Export the actions.
 export const { saveShortenedUrl, setLoading } = shortenerSlice.actions;
 
+// Export the store shortener slice reducer.
 export default shortenerSlice.reducer;
