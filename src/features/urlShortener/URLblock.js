ae575f86c228780e1be4e43b86f8e3c94c9f2b15
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-03-17 21:29:12 -0400

    Improved the delete icon.

--- src/features/urlShortener/URLblock.js
+++ src/features/urlShortener/URLblock.js
@@ -26,14 +26,20 @@ class URLblock extends React.Component {
   }
 
   delete() {
-    this.props.deleteShortenedUrl(this.props.index)
+    this.props.deleteShortenedUrl(this.props.index);
   }
 
   render() {
     return (
       <div className="urlBlock">
         <button className="deleteButton" onClick={this.delete}>
-          x
+          <svg
+            className="x"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 320 512"
+          >
+            <path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z" />
+          </svg>
         </button>
         <p className="originalUrl">{this.props.originalUrl}</p>
         <p className="shortenedUrl">{this.props.shortenedUrl}</p>
@@ -54,10 +60,10 @@ URLblock.propTypes = {
 // into the component as props. In case of URLblock, we want to be able to
 // send a deleteShortenedUrl action to the store.
 // Docs: https://react-redux.js.org/using-react-redux/connect-mapdispatch
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    deleteShortenedUrl: index => dispatch(deleteShortenedUrl(index))
-  }
-}
+    deleteShortenedUrl: (index) => dispatch(deleteShortenedUrl(index)),
+  };
+};
 
 export default connect(null, mapDispatchToProps)(URLblock);
