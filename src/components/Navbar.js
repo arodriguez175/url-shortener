d269cfa8faa06078aeaa384741a81563e11ea5c1
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-06-25 00:11:10 -0400

    Removed unused console.log and organized folder structure for readability.

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import logo from "../images/logo.svg";
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul className="navbar">
+          <li id="logo">
+            <Link to="/">
+              <img src={logo} alt="Shortly logo" />
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/features">Features</Link>
+          </li>
+          <li>
+            <Link to="/pricing">Pricing</Link>
+          </li>
+          <li>
+            <Link to="/resources">Resources</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
