8d0645779e662a0a9f684ef54fef31ffeda9ffe1
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-09-22 17:14:24 -0400

    Added github icon in Layout component.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 class Layout extends React.Component {
   render() {
     return (
       <div>
+        <a
+          href="https://github.com/arodriguez175/url-shortener"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faGithub}
+            className="text-[25px] text-primary"
+          />
+        </a>
         <Navbar />
         <Outlet />
       </div>
